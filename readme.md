d29cf97e52514f4226c1d12b19d5f740767b4f74
mrtnsch <schwarzm1995@gmail.com>
2023-07-19 08:52:05 +0200

    in-memory url map

--- readme.md
+++ readme.md
@@ -4,3 +4,45 @@ init repo
 
 add dependencies
 `cargo add dotenv sea-orm actix-web`
+
+Create hello world actix server
+```
+//src/main.rs
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+```
+
+post route that shortens url
+```
+#[post("/url")]
+async fn shorten_url(params: web::Query<UrlParameters>) -> impl Responder {
+    HttpResponse::Ok().body(params.original_url.to_owned())
+}
+```
+
+* implement in-memory url map
+
+* share via app state
+* get route which resolves to original url
+* Now we have a basic url shortener
+* introduce persistence in db
+* add seaorm, generate migrations
+* replace in-memory map with migrations
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,83 @@
+use std::collections::HashMap;
+use std::sync::Mutex;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use serde::Deserialize;
+use uuid::Uuid;
+use dotenv::dotenv;
+use std::env;
+
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[derive(Deserialize)]
+struct UrlInfoBody {
+    original_url: String,
+}
+
+#[post("/shorten")]
+async fn shorten_url(body: web::Form<UrlInfoBody>, app_state: web::Data<AppState>) -> impl Responder {
+    let server = &app_state.server;
+
+    let shortened_url = generate_short_text();
+    let mut url_map = app_state.url_map.lock().unwrap();
+    url_map.insert(shortened_url.to_string(), body.original_url.to_owned());
+
+    let response_body = format!("Your shortened URL is: http://{}/{}", server, shortened_url);
+    HttpResponse::Ok().body(response_body)
+}
+
+#[get("/{shorturl}")]
+async fn resolve_shortened_url(short_url: web::Path<String>, app_state: web::Data<AppState>) -> impl Responder {
+    let url_map = app_state.url_map.lock().unwrap();
+    let result = url_map.get(&short_url.into_inner());
+
+    match result {
+        Some(target_url) => HttpResponse::TemporaryRedirect()
+            .insert_header(("Location", target_url.to_string()))
+            .finish(),
+        None => HttpResponse::NotFound().body("URL not found"),
+    }
+
+}
+
+fn generate_short_text() -> String {
+    Uuid::new_v4().to_string().chars().take(8).collect()
+}
+
+struct AppState {
+    url_map: Mutex<HashMap<String, String>>,
+    server: String
+}
+
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+    let host = env::var("HOST").expect("Host has to be defined in env variables");
+    let port = env::var("PORT").expect("Port has to be defined in env variables");
+
+    let app_state = web::Data::new(AppState {
+        url_map: Mutex::new(HashMap::new()),
+        server: env::var("SERVER").expect("Server has to be defined in env variables")
+    });
+
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("Debug"));
+
+    HttpServer::new(move || {
+        //move is used so closure takes ownership of app_state
         App::new()
             .service(hello)
+            .service(shorten_url)
+            .service(resolve_shortened_url)
+            .app_data(app_state.clone())
+            .wrap(Logger::default())
     })
-        .bind("127.0.0.1:8000")?
+        .bind(format!("{}:{}", host, port))?
         .run()
         .await
 }
