a29ef0f89f3d04e05c485a49912e24396e28b8f9
mrtnsch <schwarzm1995@gmail.com>
2023-07-19 11:52:11 +0200

    removed unnecessary content from lib.rs in entities

--- entity/src/lib.rs
+++ entity/src/lib.rs
@@ -1,17 +1,1 @@
-//mod entities;
-//pub use entities::*;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod url_store;
