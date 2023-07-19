1ca1f55152efd6cd490897d452d4f12d0e90affc
mrtnsch <schwarzm1995@gmail.com>
2023-07-19 11:24:19 +0200

    added migration to create urlStore

--- migration/src/m20230719_090631_create_url_map_table.rs
+++ migration/src/m20230719_090631_create_url_map_table.rs
@@ -3,37 +3,48 @@ use sea_orm_migration::prelude::*;
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
+#[derive(Iden)]
+enum UrlStore {
+    Table,
+    Id,
+    ShortUrl,
+    TargetUrl,
+    CreatedAt
+}
+
+
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
 
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
-                    .if_not_exists()
-                    .col(
-                        ColumnDef::new(Post::Id)
-                            .integer()
-                            .not_null()
-                            .auto_increment()
-                            .primary_key(),
+                        .table(UrlStore::Table)
+                        .if_not_exists()
+                        .col(
+                            ColumnDef::new(UrlStore::Id)
+                                .integer()
+                                .not_null()
+                                .auto_increment()
+                                .primary_key(),
                     )
-                    .col(ColumnDef::new(Post::Title).string().not_null())
-                    .col(ColumnDef::new(Post::Text).string().not_null())
-                    .to_owned(),
+                    .col(
+                        ColumnDef::new(UrlStore::ShortUrl)
+                            .string()
+                            .not_null()
+                    )
+                    .col(ColumnDef::new(UrlStore::TargetUrl).string().not_null())
+                    .col(ColumnDef::new(UrlStore::CreatedAt).timestamp().not_null().default(SimpleExpr::Keyword(Keyword::CurrentTimestamp)))
+                    .to_owned()
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
 
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(UrlStore::Table).to_owned())
             .await
     }
 }
